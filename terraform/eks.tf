a69b0a76a999dac0c4305d4a9231ae7efbe99ea4
murthylinux <murthy09241@gmail.com>
2024-12-19 22:59:50 +0530

    modified the eks module version

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,6 +1,6 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "1.27"
+  version = "~> 20.31"
 
   cluster_name                   = local.name
   cluster_endpoint_public_access = true
